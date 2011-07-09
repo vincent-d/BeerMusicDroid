d83d28057c620e4eaf58d1f770d33875a443a61d
Vincent Dupont <vincent.touffi@gmail.com>
2011-07-09 21:57:13 +0200

    Split BeerSoundAnalyzer in different functions

--- src/eu/silvere/BeerProjectActivity.java
+++ src/eu/silvere/BeerProjectActivity.java
@@ -2,16 +2,25 @@ package eu.silvere;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class BeerProjectActivity extends Activity {
+	
+	private BeerSoundAnalyzer mBeerAnalyze;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    	super.onCreate(savedInstanceState);
+    	Log.d("Beer", "Beer" + "Activity started");
         TextView tv = new TextView(this);
         tv.setText("Hello, Beeeeeeeeeeer");
         setContentView(tv);
 
+        mBeerAnalyze = new BeerSoundAnalyzer();
+		mBeerAnalyze.launchRecording();
+		float f = mBeerAnalyze.computeFFT();
+		tv.setText("Hello, Beeeeeeeeeeer " + f);
     }
 }
--- src/eu/silvere/BeerSoundAnalyzer.java
+++ src/eu/silvere/BeerSoundAnalyzer.java
@@ -8,79 +8,86 @@ import android.media.AudioRecord;
 import android.media.MediaRecorder.AudioSource;
 import android.util.FloatMath;
 import android.util.Log;
-import android.widget.SlidingDrawer;
 import edu.emory.mathcs.jtransforms.fft.FloatFFT_1D;
-
 
 /**
  * @author Vincent Dupont <vincent.touffi@gmail.com>
- *
+ * 
  */
 public class BeerSoundAnalyzer {
-	
-	/** 
+
+	/**
 	 * The sampling frequency of the record
 	 */
 	private static final int SAMPLING_FREQ = 44100;
-	/** 
+	/**
 	 * The maximum frequency
 	 */
 	private static final int MAX_FREQ = 1000;
-	/** 
+	/**
 	 * The duration of record
 	 */
 	private static final int RECORD_DURATION = 3;
-	
-	
+
 	private FloatFFT_1D mFFT;
 	private AudioRecord mRecord;
 	private byte[] mRawDataMic;
 	private float[] mSoundData;
-	
+
 	public BeerSoundAnalyzer() {
-		
-		int bufSize = AudioRecord.getMinBufferSize(SAMPLING_FREQ, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
-		bufSize = RECORD_DURATION * SAMPLING_FREQ < bufSize ? bufSize : RECORD_DURATION * SAMPLING_FREQ;
+
+		int bufSize = AudioRecord.getMinBufferSize(SAMPLING_FREQ, AudioFormat.CHANNEL_IN_MONO,
+				AudioFormat.ENCODING_PCM_16BIT);
+		bufSize = RECORD_DURATION * SAMPLING_FREQ < bufSize ? bufSize : RECORD_DURATION
+				* SAMPLING_FREQ;
 		Log.d("Beer", "Beer " + "bufSize " + bufSize);
-		mRecord = new AudioRecord(AudioSource.MIC, SAMPLING_FREQ,AudioFormat.CHANNEL_IN_MONO, 
+		mRecord = new AudioRecord(AudioSource.MIC, SAMPLING_FREQ, AudioFormat.CHANNEL_IN_MONO,
 				AudioFormat.ENCODING_PCM_16BIT, bufSize);
-		
+
 		mRawDataMic = new byte[bufSize];
-		mSoundData = new float[bufSize] ;
-		
+		mSoundData = new float[bufSize];
+
 		mFFT = new FloatFFT_1D(bufSize);
-		
+
 	}
-	
-	
+
 	public void launchRecording() {
-		
-		int ret ;
-		float maxNorm = 0;
-		float norm;
-		float freq = 0;
-		
-		
+
+		int ret;
+
 		Log.d("Beer", "Beer " + mRawDataMic.length + " Recording");
-		
+
 		mRecord.startRecording();
 		ret = mRecord.read(mRawDataMic, 0, mRawDataMic.length);
 		mRecord.stop();
+
 		
-		for (int i = 0 ; i < ret ; i++)
+
+	}
+
+	public float computeFFT() {
+
+		float maxNorm = 0;
+		float norm = 0;
+		float freq = 0;
+
+		for (int i = 0; i < mSoundData.length ; i++)
 			mSoundData[i] = mRawDataMic[i];
 		
 		mFFT.realForward(mSoundData);
-		maxNorm = norm = 0;
-		for (int i = 2 ; i < (MAX_FREQ * mSoundData.length / SAMPLING_FREQ) - 1 ; i++) {
-			norm = FloatMath.sqrt(mSoundData[i] * mSoundData[i] + mSoundData[i+1] * mSoundData[i+1]);
+
+		for (int i = 2; i < (MAX_FREQ * mSoundData.length / SAMPLING_FREQ) - 1; i++) {
+			norm = FloatMath.sqrt(mSoundData[i] * mSoundData[i] + mSoundData[i + 1]
+					* mSoundData[i + 1]);
 			if (maxNorm < norm) {
-				maxNorm = norm ;
+				maxNorm = norm;
 				freq = SAMPLING_FREQ * i / (mSoundData.length);
 			}
 		}
-		Log.d("Beer", "Beer max norm " + maxNorm + " freq " + freq);
+		Log.d("Beer", "Beer freq " + freq);
+
+		return freq;
+
 	}
 
-	
 }
