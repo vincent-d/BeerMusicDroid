61aa054b8839d91ff84bb9a5f05d5741ea52d06f
Vincent Dupont <vincent.touffi@gmail.com>
2011-12-15 19:47:30 +0100

    Update xml view
    This is not the final interface

--- gen/eu/silvere/R.java
+++ gen/eu/silvere/R.java
@@ -15,17 +15,23 @@ public final class R {
         public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int analyzingProgressBar=0x7f060004;
-        public static final int button=0x7f060005;
-        public static final int description=0x7f060001;
-        public static final int launch=0x7f060002;
-        public static final int name=0x7f060000;
-        public static final int text=0x7f060003;
+        public static final int RelativeLayout1=0x7f060005;
+        public static final int TableLayout1=0x7f060000;
+        public static final int analyzingProgressBar=0x7f060007;
+        public static final int button=0x7f060008;
+        public static final int description=0x7f060004;
+        public static final int imageView1=0x7f060009;
+        public static final int launch=0x7f060001;
+        public static final int name=0x7f060003;
+        public static final int progressBar1=0x7f060002;
+        public static final int text=0x7f060006;
     }
     public static final class layout {
         public static final int beer_list_activity_view=0x7f030000;
-        public static final int beer_main_activity_view=0x7f030001;
-        public static final int main=0x7f030002;
+        public static final int beer_list_activity_view_2=0x7f030001;
+        public static final int beer_main_activity_view=0x7f030002;
+        public static final int beer_row_view=0x7f030003;
+        public static final int main=0x7f030004;
     }
     public static final class string {
         public static final int appTitle=0x7f050002;
